faef2f10dc87eed127732ac78dcab1009d2b30da
Gathin <gathint23@gmail.com>
2023-10-26 12:21:00 +0530

    Upd: Onboarding Step-1 with redirection

--- src/components/Home/CreateWorkerId.tsx
+++ src/components/Home/CreateWorkerId.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import TalentLayerContext from '../../context/talentLayer';
-import TalentLayerIdForm from '../Form/TalentLayerIdForm';
 
 function CreateWorkerId() {
   const { user } = useContext(TalentLayerContext);
@@ -14,17 +13,17 @@ function CreateWorkerId() {
       <div className='bg-white'>
         <div className='max-w-7xl mx-auto text-stone-800 sm:px-4 lg:px-0 py-20'>
           <div className='flex flex-col items-center justify-center gap-10'>
-            <p className='text-5xl sm:text-7xl font-medium tracking-wider max-w-lg text-center'>
+            <p className='text-5xl sm:text-7xl font-medium tracking-wider max-w-5xl text-center'>
               Create Your self-owned worker identity
             </p>
 
             <p className='text-stone-400 text-center'>
-            Details about TLIDs and benefits to hirers and workers, on chain reputation and other details about this.
+              Details about TLIDs and benefits to hirers and workers, on chain reputation and other
+              details about this.
               <br />
-            Explain how blockchain/wallets let you own your reputation fovever. Maybe link to an other info center about benfits.
+              Explain how blockchain/wallets let you own your reputation fovever. Maybe link to an
+              other info center about benfits.
             </p>
-
-            <TalentLayerIdForm />
           </div>
         </div>
       </div>
--- src/pages/landing/worker-onboarding/index.tsx
+++ src/pages/landing/worker-onboarding/index.tsx
@@ -1,9 +1,31 @@
-import CreateWorkerId from "../../../components/Home/CreateWorkerId";
+import { useContext, useEffect } from 'react';
+import CreateWorkerId from '../../../components/Home/CreateWorkerId';
+import Steps from '../../../components/Steps';
+import TalentLayerContext from '../../../context/talentLayer';
+import Loading from '../../../components/Loading';
+import { useRouter } from 'next/router';
 
 function workerOnboardingStep1() {
+  const { account, loading, user } = useContext(TalentLayerContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (account?.isConnected && user) {
+      router.push('/worker-onboarding/step2');
+    }
+  }, [account, user, router]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <CreateWorkerId />
+      <div className='flex flex-col items-center justify-center gap-10'>
+        <Steps />
+      </div>
     </>
   );
 }
