d82ae40caa3193b8e45f40c63e7524332ebc70f8
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-10-29 11:01:00 +0000

    ci: fixed build error and missing imports

--- src/pages/landing/onboarding/step3.tsx
+++ src/pages/landing/onboarding/step3.tsx
@@ -1,15 +1,16 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useChainId, useWalletClient } from 'wagmi';
 import * as Yup from 'yup';
+import HirerProfileLayout from '../../../components/HirerProfileLayout';
+import Loading from '../../../components/Loading';
 import TalentLayerContext from '../../../context/talentLayer';
 import { useGetBuilderPlaceFromOwner } from '../../../modules/BuilderPlace/hooks/UseGetBuilderPlaceFromOwner';
 import { useUpdateBuilderPlace } from '../../../modules/BuilderPlace/hooks/UseUpdateBuilderPlace';
 import { upload } from '../../../modules/BuilderPlace/request';
 import { iBuilderPlacePalette } from '../../../modules/BuilderPlace/types';
 import { themes } from '../../../utils/themes';
-
 interface IFormValues {
   subdomain: string;
   palette: keyof typeof themes;
@@ -36,8 +37,6 @@ function onboardingStep3() {
   const initialValues: IFormValues = {
     subdomain: builderPlaceData?.subdomain || '',
     palette: 'light',
-    logo: builderPlaceData?.logo || '',
-    cover: builderPlaceData?.cover || '',
   };
 
   if (loading) {
@@ -63,11 +62,22 @@ function onboardingStep3() {
           message: values.subdomain,
         });
 
+        let logo;
+        if (values.logo) {
+          logo = await upload(values.logo);
+          console.log({ logo, url: logo?.variants[0] });
+        }
+        let cover;
+        if (values.cover) {
+          cover = await upload(values.cover);
+          console.log({ cover, url: cover?.variants[0] });
+        }
+
         if (builderPlaceData) {
           await updateBuilderPlaceAsync({
             subdomain: values.subdomain,
-            logo: values.logo || undefined,
-            cover: values.cover || undefined,
+            logo: logo?.variants[0] || null,
+            cover: cover?.variants[0] || null,
             name: builderPlaceData.name,
             ownerTalentLayerId: builderPlaceData.ownerTalentLayerId,
             palette: themes[values.palette],
@@ -109,63 +119,64 @@ function onboardingStep3() {
                 <span className='text-red-500'>
                   <ErrorMessage name='subdomain' />
                 </span>
-              <label className='block'>
-                <span className='text-stone-800'>Choose a Color Palette</span>
+                <label className='block'>
+                  <span className='text-stone-800'>Choose a Color Palette</span>
 
-                <div className='flex flex-col gap-2'>
-                  {Object.keys(themes).map((value, index) => {
-                    return (
-                      <div>
-                        <input
-                          type='radio'
-                          className='hidden peer'
-                          name='palette'
-                          id={`palette-${index}`}
-                          key={value}
-                          onChange={() => {
-                            setFieldValue('palette', value);
-                          }}
-                        />
-                        <label>{value} Palette</label>
-                        <label
-                          htmlFor={`palette-${index}`}
-                          className='h-24 peer-checked:border-blue-500 border-2 border-solid rounded-lg flex items-center px-1 gap-2 w-fit'>
-                          {Object.keys(themes[value as keyof typeof themes]).map(color => {
-                            return (
-                              <div
-                                className='group relative inline-block w-20 h-20 border'
-                                style={{
-                                  backgroundColor:
-                                    themes[value as keyof typeof themes][
-                                      color as keyof iBuilderPlacePalette
-                                    ],
-                                }}>
-                                <span className="absolute hidden group-hover:flex -top-2 -right-3 translate-x-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm before:content-[''] before:absolute before:top-1/2  before:right-[100%] before:-translate-y-1/2 before:border-8 before:border-y-transparent before:border-l-transparent before:border-r-gray-700 z-50">
-                                  {color} <br />
-                                  {
-                                    themes[value as keyof typeof themes][
-                                      color as keyof iBuilderPlacePalette
-                                    ]
-                                  }
-                                </span>
-                              </div>
-                            );
-                          })}
-                        </label>
-                      </div>
-                    );
-                  })}
-                </div>
-              </label>
-              <button
-                type='submit'
-                className='grow px-5 py-2 rounded-xl bg-redpraha text-stone-800'>
-                I'm done
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+                  <div className='flex flex-col gap-2'>
+                    {Object.keys(themes).map((value, index) => {
+                      return (
+                        <div>
+                          <input
+                            type='radio'
+                            className='hidden peer'
+                            name='palette'
+                            id={`palette-${index}`}
+                            key={value}
+                            onChange={() => {
+                              setFieldValue('palette', value);
+                            }}
+                          />
+                          <label>{value} Palette</label>
+                          <label
+                            htmlFor={`palette-${index}`}
+                            className='h-24 peer-checked:border-blue-500 border-2 border-solid rounded-lg flex items-center px-1 gap-2 w-fit'>
+                            {Object.keys(themes[value as keyof typeof themes]).map(color => {
+                              return (
+                                <div
+                                  className='group relative inline-block w-20 h-20 border'
+                                  style={{
+                                    backgroundColor:
+                                      themes[value as keyof typeof themes][
+                                        color as keyof iBuilderPlacePalette
+                                      ],
+                                  }}>
+                                  <span className="absolute hidden group-hover:flex -top-2 -right-3 translate-x-full w-auto px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm before:content-[''] before:absolute before:top-1/2  before:right-[100%] before:-translate-y-1/2 before:border-8 before:border-y-transparent before:border-l-transparent before:border-r-gray-700 z-50">
+                                    {color} <br />
+                                    {
+                                      themes[value as keyof typeof themes][
+                                        color as keyof iBuilderPlacePalette
+                                      ]
+                                    }
+                                  </span>
+                                </div>
+                              );
+                            })}
+                          </label>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </label>
+                <button
+                  type='submit'
+                  className='grow px-5 py-2 rounded-xl bg-redpraha text-stone-800'>
+                  I'm done
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </HirerProfileLayout>
     </>
   );
 }
