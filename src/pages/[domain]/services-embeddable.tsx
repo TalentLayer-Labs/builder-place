f9d96a80d9f5840fb6089241addabd51d6dda8f5
Quentin D.C <quent043@hotmail.com>
2023-11-09 11:04:59 +0000

    Removed query params

--- src/pages/[domain]/services-embeddable.tsx
+++ src/pages/[domain]/services-embeddable.tsx
@@ -1,17 +1,12 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import { ServiceStatusEnum } from '../../types';
 import ServicesEmbed from './services-embed';
 
 const ServicesEmbeddable = () => {
   const router = useRouter();
-
   const boardTitle = (router.query?.title as string) || 'My Gig board';
-  const buyerId = (router.query?.buyerId as string) || '0';
-  const boardSkill = router.query?.status || 'Opened';
-  const boardStatusEnum = ServiceStatusEnum[boardSkill as keyof typeof ServiceStatusEnum];
 
-  return <ServicesEmbed title={boardTitle} buyerId={buyerId} status={boardStatusEnum} />;
+  return <ServicesEmbed title={boardTitle} />;
 };
 
 export default ServicesEmbeddable;
