860fc52bbf291fbd78d13ff137393066bc2e4681
Quentin D.C <quent043@hotmail.com>
2023-11-09 10:35:57 +0000

    Finalized embed page

--- src/pages/[domain]/admin/embed-place.tsx
+++ src/pages/[domain]/admin/embed-place.tsx
@@ -4,7 +4,7 @@ import TalentLayerContext from '../../../context/talentLayer';
 import BuilderPlaceContext from '../../../modules/BuilderPlace/context/BuilderPlaceContext';
 import useCopyToClipBoard from '../../../hooks/useCopyToClipBoard';
 import { ClipboardCopy, CheckCircle } from 'heroicons-react';
-import Link from 'next/link';
+import Loading from '../../../components/Loading';
 
 export async function getServerSideProps({ params }: any) {
   return await getBuilderPlace(params.domain);
@@ -22,101 +22,100 @@ const generateServicesEmbedIframeCode = (servicesEmbedUrl: string): string => {
 };
 
 export default function EmbedPlace() {
-  const { user } = useContext(TalentLayerContext);
+  const { user, loading } = useContext(TalentLayerContext);
   const { builderPlace, isBuilderPlaceOwner } = useContext(BuilderPlaceContext);
   const { isCopied: isIframeCopied, copyToClipboard: copyIframe } = useCopyToClipBoard();
   const { isCopied: isDomainCopied, copyToClipboard: copyDomain } = useCopyToClipBoard();
 
-  if (!isBuilderPlaceOwner) {
+  if (loading) {
     return (
-      <>
+      <div className='flex justify-center items-center gap-10 flex-col pb-5 mt-5'>
+        <Loading />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {isBuilderPlaceOwner && (
         <div className='max-w-7xl mx-auto text-base-content'>
           <div className='-mx-6 -mt-6 sm:mx-0 sm:mt-0'>
             <div className='flex py-2 px-6 sm:px-0 items-center w-full mb-8'>
-              <p className='text-2xl font-bold flex-1 mt-6'>You are not an admin of this domain</p>
+              <p className='text-2xl font-bold flex-1 mt-6'>Embed your place</p>
+            </div>
+          </div>
+          <div className='flex flex-row justify-center gap-2'>
+            <div className={'flex-1 border border-gray-700 rounded-xl'}>
+              <p className={'text-xl font-bold flex-1 mt-6'}>Embeddable job board</p>
+              <p>embed your job board on a page in your website. read the guide</p>
+              <div className={'flex flex-row gap-2'}>
+                <code className={'basis-5/6 border border-t-gray-500 rounded-xl p-2'}>
+                  {user?.id && generateServicesEmbedIframeCode(generateServicesEmbedUrl(user.id))}
+                </code>
+                {!isIframeCopied ? (
+                  <span
+                    className={
+                      'flex flex-row basis-1/6 items-center border-l border-t-gray-500 ml-2 px-2 py-1 hover:rounded-xl hover:cursor-pointer hover:bg-gray-200 hover:transition-all duration-150'
+                    }
+                    onClick={() =>
+                      user?.id &&
+                      copyIframe(generateServicesEmbedIframeCode(generateServicesEmbedUrl(user.id)))
+                    }>
+                    <ClipboardCopy />
+                    <p className={'text-sm text-gray-500'}>Copy</p>
+                  </span>
+                ) : (
+                  <span
+                    className={
+                      'flex flex-row basis-1/6 items-center border-l border-t-green-500 ml-2 px-2 py-1 rounded-xl bg-green-200'
+                    }>
+                    <CheckCircle />
+                    <p className={'text-sm text-gray-500'}>Copied !</p>
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className={'flex-1 border border-gray-700 rounded-xl'}>
+              <p className={'text-xl font-bold flex-1 mt-6'}>Shareable board</p>
+              <p>share your hosted work board with your community</p>
+              <div className={'flex flex-row gap-2'}>
+                <p className={'basis-5/6 border border-t-gray-500 rounded-full p-2'}>
+                  {builderPlace?.customDomain || builderPlace?.subdomain}
+                </p>
+                {!isDomainCopied && (builderPlace?.customDomain || builderPlace?.subdomain) ? (
+                  <span
+                    className={
+                      'flex flex-row basis-1/6 items-center border-l border-t-gray-500 ml-2 px-2 py-1 hover:rounded-full hover:cursor-pointer hover:bg-gray-200 hover:transition-all duration-150'
+                    }
+                    onClick={() =>
+                      copyDomain((builderPlace?.customDomain || builderPlace?.subdomain) as string)
+                    }>
+                    <ClipboardCopy />
+                    <p className={'text-sm text-gray-500'}>Copy</p>
+                  </span>
+                ) : (
+                  <span
+                    className={
+                      'flex flex-row basis-1/6 items-center border-l border-t-green-500 ml-2 px-2 py-1 rounded-full bg-green-200'
+                    }>
+                    <CheckCircle />
+                    <p className={'text-sm text-gray-500'}>Copied !</p>
+                  </span>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-  return (
-    <>
-      <div className='max-w-7xl mx-auto text-base-content'>
-        <div className='-mx-6 -mt-6 sm:mx-0 sm:mt-0'>
-          <div className='flex py-2 px-6 sm:px-0 items-center w-full mb-8'>
-            <p className='text-2xl font-bold flex-1 mt-6'>Embed your place</p>
-          </div>
+      )}
+      {isBuilderPlaceOwner && (
+        <div className={'flex flex-col items-center justify-center mt-4'}>
+          <h1 className='text-title text-4xl mb-4 text-center'>Work Board Preview</h1>
+          <iframe
+            src='http://mongo-boss.localhost:3000/services-embeddable?buyerId=172'
+            width='600'
+            height='400'></iframe>
         </div>
-        <div className='flex flex-row justify-center gap-2'>
-          <div className={'flex-1 border border-gray-700 rounded-xl'}>
-            <p className={'text-xl font-bold flex-1 mt-6'}>Embeddable job board</p>
-            <p>embed your job board on a page in your website. read the guide</p>
-            <div className={'flex flex-row gap-2'}>
-              <code className={'basis-5/6 border border-t-gray-500 rounded-xl p-2'}>
-                {user?.id && generateServicesEmbedIframeCode(generateServicesEmbedUrl(user.id))}
-              </code>
-              {!isIframeCopied ? (
-                <span
-                  className={
-                    'flex flex-row basis-1/6 items-center border-l border-t-gray-500 ml-2 px-2 py-1 hover:rounded-xl hover:cursor-pointer hover:bg-gray-200 hover:transition-all duration-150'
-                  }
-                  onClick={() =>
-                    user?.id &&
-                    copyIframe(generateServicesEmbedIframeCode(generateServicesEmbedUrl(user.id)))
-                  }>
-                  <ClipboardCopy />
-                  <p className={'text-sm text-gray-500'}>Copy</p>
-                </span>
-              ) : (
-                <span
-                  className={
-                    'flex flex-row basis-1/6 items-center border-l border-t-green-500 ml-2 px-2 py-1 rounded-xl bg-green-200'
-                  }>
-                  <CheckCircle />
-                  <p className={'text-sm text-gray-500'}>Copied !</p>
-                </span>
-              )}
-            </div>
-          </div>
-          <div className={'flex-1 border border-gray-700 rounded-xl'}>
-            <p className={'text-xl font-bold flex-1 mt-6'}>Shareable board</p>
-            <p>share your hosted work board with your community</p>
-            <div className={'flex flex-row gap-2'}>
-              <p className={'basis-5/6 border border-t-gray-500 rounded-full p-2'}>
-                {builderPlace?.customDomain || builderPlace?.subdomain}
-              </p>
-              {!isDomainCopied && (builderPlace?.customDomain || builderPlace?.subdomain) ? (
-                <span
-                  className={
-                    'flex flex-row basis-1/6 items-center border-l border-t-gray-500 ml-2 px-2 py-1 hover:rounded-full hover:cursor-pointer hover:bg-gray-200 hover:transition-all duration-150'
-                  }
-                  onClick={() =>
-                    copyDomain((builderPlace?.customDomain || builderPlace?.subdomain) as string)
-                  }>
-                  <ClipboardCopy />
-                  <p className={'text-sm text-gray-500'}>Copy</p>
-                </span>
-              ) : (
-                <span
-                  className={
-                    'flex flex-row basis-1/6 items-center border-l border-t-green-500 ml-2 px-2 py-1 rounded-full bg-green-200'
-                  }>
-                  <CheckCircle />
-                  <p className={'text-sm text-gray-500'}>Copied !</p>
-                </span>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={'flex flex-col items-center justify-center mt-4'}>
-        <h1 className='text-title text-4xl mb-4 text-center'>Work Board Preview</h1>
-        <iframe
-          src='http://mongo-boss.localhost:3000/services-embeddable?buyerId=172'
-          width='600'
-          height='400'></iframe>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
