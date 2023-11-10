3185315ccc83e86c76f6a5c46e1d4196b5a25db6
Quentin D.C <quent043@hotmail.com>
2023-11-10 15:03:05 +0000

    Added a secure route

--- src/pages/[domain]/profiles/edit/privacy.tsx
+++ src/pages/[domain]/profiles/edit/privacy.tsx
@@ -1,12 +1,21 @@
 import Layout from '../../../../components/EditProfile/Layout';
 import { getBuilderPlace } from '../../../../modules/BuilderPlace/queries';
 import Web3mailPreferencesForm from '../../../../modules/Web3mail/components/Web3mailPreferencesForm';
+import React, { useContext } from 'react';
+import BuilderPlaceContext from '../../../../modules/BuilderPlace/context/BuilderPlaceContext';
+import AccessDenied from '../../../../components/AccessDenied';
 
 export async function getServerSideProps({ params }: any) {
   return await getBuilderPlace(params.domain);
 }
 
 function EditPrivacy() {
+  const { isBuilderPlaceOwner } = useContext(BuilderPlaceContext);
+
+  if (!isBuilderPlaceOwner) {
+    return <AccessDenied />;
+  }
+
   return (
     <Layout>
       <Web3mailPreferencesForm />
