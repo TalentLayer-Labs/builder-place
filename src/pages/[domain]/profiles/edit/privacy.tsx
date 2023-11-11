e75b825e0d801f0fb1a388ddcf4b2f219b70cc46
Romain <romain@talentlayer.org>
2023-11-11 15:59:04 +0100

    Update privacy.tsx

--- src/pages/[domain]/profiles/edit/privacy.tsx
+++ src/pages/[domain]/profiles/edit/privacy.tsx
@@ -1,21 +1,12 @@
 import Layout from '../../../../components/EditProfile/Layout';
 import { getBuilderPlace } from '../../../../modules/BuilderPlace/queries';
 import Web3mailPreferencesForm from '../../../../modules/Web3mail/components/Web3mailPreferencesForm';
-import { useContext } from 'react';
-import BuilderPlaceContext from '../../../../modules/BuilderPlace/context/BuilderPlaceContext';
-import AccessDenied from '../../../../components/AccessDenied';
 
 export async function getServerSideProps({ params }: any) {
   return await getBuilderPlace(params.domain);
 }
 
 function EditPrivacy() {
-  const { isBuilderPlaceOwner } = useContext(BuilderPlaceContext);
-
-  if (!isBuilderPlaceOwner) {
-    return <AccessDenied />;
-  }
-
   return (
     <Layout>
       <Web3mailPreferencesForm />
