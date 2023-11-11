ffb25a8bf7df3cd6d76381ae54f58bfdd555ff3d
Romain <romain@talentlayer.org>
2023-11-11 15:58:49 +0100

    remove security on user section page

--- src/pages/[domain]/profiles/edit/index.tsx
+++ src/pages/[domain]/profiles/edit/index.tsx
@@ -1,21 +1,12 @@
 import ProfileForm from '../../../../components/Form/ProfileForm';
 import Layout from '../../../../components/EditProfile/Layout';
 import { getBuilderPlace } from '../../../../modules/BuilderPlace/queries';
-import BuilderPlaceContext from '../../../../modules/BuilderPlace/context/BuilderPlaceContext';
-import { useContext } from 'react';
-import AccessDenied from '../../../../components/AccessDenied';
 
 export async function getServerSideProps({ params }: any) {
   return await getBuilderPlace(params.domain);
 }
 
 function EditProfile() {
-  const { isBuilderPlaceOwner } = useContext(BuilderPlaceContext);
-
-  if (!isBuilderPlaceOwner) {
-    return <AccessDenied />;
-  }
-
   return (
     <Layout>
       <ProfileForm />
