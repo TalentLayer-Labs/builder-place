5dd58596f209b8ee4a8d3cf0cb140bf5cbc6a898
Quentin D.C <quent043@hotmail.com>
2023-12-16 11:35:07 +0000

    Removed comments

--- src/modules/BuilderPlace/hooks/UseSetWorkerProfileOwner.ts
+++ src/modules/BuilderPlace/hooks/UseSetWorkerProfileOwner.ts
@@ -1,10 +1,7 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { SetWorkerProfileOwner } from '../types';
-import { showMongoErrorTransactionToast } from '../../../utils/toast';
 
 export function useSetWorkerProfileOwner() {
-  const queryClient = useQueryClient();
-
   return useMutation<{ message: string; id: string; error?: string }, Error, SetWorkerProfileOwner>(
     (updateWorkerProfileData: SetWorkerProfileOwner) =>
       fetch('/api/domain/set-worker-profile-owner', {
@@ -14,20 +11,7 @@ export function useSetWorkerProfileOwner() {
           'Content-type': 'application/json',
         },
       }).then(res => {
-        // if (res.status === 200 || res.status === 401) {
         return res.json();
-        // } else {
-        //   res
-        //     .json()
-        //     .then((data: any) => {
-        //       console.log('data', data);
-        //       showMongoErrorTransactionToast(data.error);
-        //       return data.error;
-        //     })
-        //     .catch(err => {
-        //       throw new Error('Failed to update Worker Profile', err.fileName);
-        //     });
-        // }
       }),
   );
 }
