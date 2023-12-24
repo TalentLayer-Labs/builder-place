6b600567efd5e40735edaf857c4751b4d69196fc
Quentin D.C <quent043@hotmail.com>
2023-12-24 13:04:30 +0000

    Replaced hood by direct call

--- src/modules/BuilderPlace/context/BuilderPlaceContext.tsx
+++ src/modules/BuilderPlace/context/BuilderPlaceContext.tsx
@@ -1,7 +1,9 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import { IBuilderPlace } from '../types';
 import { useAccount } from 'wagmi';
-import useUserById from '../../../hooks/useUserById';
+import { getUserById } from '../../../queries/users';
+import { toast } from 'react-toastify';
+import { useChainId } from '../../../hooks/useChainId';
 
 const BuilderPlaceContext = createContext<{
   builderPlace?: IBuilderPlace;
@@ -15,9 +17,47 @@ const BuilderPlaceContext = createContext<{
 
 const BuilderPlaceProvider = ({ data, children }: { data: IBuilderPlace; children: ReactNode }) => {
   const account = useAccount();
+  const chainId = useChainId();
   const [builderPlace, setBuilderPlace] = useState<IBuilderPlace | undefined>();
   const [isBuilderPlaceCollaborator, setIsBuilderPlaceCollaborator] = useState<boolean>(false);
   const [isBuilderPlaceOwner, setIsBuilderPlaceOwner] = useState(false);
+
+  const fetchBuilderPlaceOwner = async () => {
+    if (!data?.ownerTalentLayerId) {
+      return;
+    }
+
+    try {
+      const response = await getUserById(chainId, data.ownerTalentLayerId);
+      if (!response?.data?.data?.user) {
+        return;
+      }
+
+      const builderPlaceOwner = response.data.data.user;
+
+      const isUserBuilderPlaceOwner =
+        account.address?.toLocaleLowerCase() === builderPlaceOwner?.address?.toLocaleLowerCase();
+      setIsBuilderPlaceOwner(isUserBuilderPlaceOwner || false);
+    } catch (err: any) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      toast.error(`An error happened while loading your account: ${err.message}.`, {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    fetchBuilderPlaceOwner();
+  }, [chainId, data?.ownerTalentLayerId]);
 
   useEffect(() => {
     if (!data || !data.ownerTalentLayerId) return;
@@ -26,13 +66,7 @@ const BuilderPlaceProvider = ({ data, children }: { data: IBuilderPlace; childre
       owner => owner.toLocaleLowerCase() === account?.address?.toLocaleLowerCase(),
     );
 
-    const builderPlaceOwner = useUserById(data.ownerTalentLayerId);
-
-    const isBuilderPlaceOwner =
-      account.address?.toLocaleLowerCase() === builderPlaceOwner?.address?.toLocaleLowerCase();
-
     setIsBuilderPlaceCollaborator(isBuilderPlaceCollaborator || false);
-    setIsBuilderPlaceOwner(isBuilderPlaceOwner || false);
     setBuilderPlace(data);
   }, [data, account]);
 
