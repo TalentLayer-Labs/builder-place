68ee56311ce3320f5ed8f1c0b2f6037d2e9215b0
Quentin D.C <quent043@hotmail.com>
2023-12-31 12:44:33 +0000

    Refactor

--- migrations/1704017512198-add-owner-address.ts
+++ migrations/1704017512198-add-owner-address.ts
@@ -6,21 +6,21 @@ const chainId = 80001;
 let builderPlacesWithOwners: any[] = [];
 export async function up(): Promise<void> {
   await connection();
+
   console.log('Getting all builderPlaces');
   const builderPlaces = await BuilderPlace.find();
   console.log('Fetched builderPlaces, ', builderPlaces.length);
   builderPlacesWithOwners = builderPlaces.filter(builderPlace => !!builderPlace.ownerTalentLayerId);
   console.log('Filtered builderPlaces with owners, ', builderPlacesWithOwners.length);
-  builderPlacesWithOwners.forEach(builderPlace => console.log(builderPlace._id));
 
   for (let builderPlace of builderPlacesWithOwners) {
     console.log('Owner TLID', builderPlace.ownerTalentLayerId);
     const owner = await getUserById(chainId, builderPlace.ownerTalentLayerId);
-    console.log('Owner Address', owner?.data?.data?.user?.address);
     if (!owner?.data?.data?.user?.address) {
       console.log('No owner found for builderPlace', builderPlace._id);
       continue;
     }
+    console.log('Owner Address', owner.data.data.user.address);
 
     // Preparing new builderPlace data
     const builderPlaceData = { ...builderPlace }._doc;
@@ -32,7 +32,7 @@ export async function up(): Promise<void> {
     };
     const serializedBuilderPlace = JSON.parse(JSON.stringify(completeBuilderPlaceData));
 
-    // Delete former builderPlace (need to first delete as required unique fields)
+    // Delete former builderPlace (need to first delete as some fields are required & unique)
     console.log('Deleting former builderPlace', builderPlaceId);
     await BuilderPlace.deleteOne({ _id: builderPlaceId }).exec();
 
