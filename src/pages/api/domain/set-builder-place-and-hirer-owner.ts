6233263dd7ef9b7b359a574f0ea2d1c6d918187f
Quentin D.C <quent043@hotmail.com>
2024-01-19 12:33:53 +0000

    Optimized API

--- src/pages/api/domain/set-builder-place-and-hirer-owner.ts
+++ src/pages/api/domain/set-builder-place-and-hirer-owner.ts
@@ -14,59 +14,62 @@ import {
   removeOwnerFromUser,
   setUserOwner,
 } from '../../../modules/BuilderPlace/actions/user';
+import { MISSING_DATA } from '../../../modules/BuilderPlace/apiResponses';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'PUT') {
-    const body: SetBuilderPlaceAndHirerOwner = req.body;
-    console.log('Received data:', body);
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
-    if (!body.builderPlaceId || !body.hirerId || !body.ownerAddress || !body.ownerTalentLayerId) {
-      return res.status(400).json({ error: 'Missing data' });
-    }
+  const body: SetBuilderPlaceAndHirerOwner = req.body;
+  console.log('Received data:', body);
 
-    if (!process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID) {
-      return res.status(500).json({ error: 'Missing default chain config' });
-    }
+  if (!body.builderPlaceId || !body.hirerId || !body.ownerAddress || !body.ownerTalentLayerId) {
+    return res.status(400).json({ error: MISSING_DATA });
+  }
 
-    /**
-     * @dev: Checks on the domain
-     */
-    const existingSpace = await getBuilderPlaceByOwnerTalentLayerId(body.ownerTalentLayerId);
+  if (!process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID) {
+    return res.status(500).json({ error: 'Missing default chain config' });
+  }
+
+  if (!body.builderPlaceId || !body.hirerId || !body.ownerAddress || !body.ownerTalentLayerId) {
+    return res.status(400).json({ error: MISSING_DATA });
+  }
+
+  try {
+    const [
+      existingSpace,
+      builderSpace,
+      talentLayerUser,
+      existingProfileWithSameAddress,
+      userProfile,
+    ] = await Promise.all([
+      getBuilderPlaceByOwnerTalentLayerId(body.ownerTalentLayerId),
+      getBuilderPlaceById(body.builderPlaceId as string),
+      getTalentLayerUserByAddress(
+        Number(process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID),
+        body.ownerAddress,
+      ),
+      getUserByAddress(body.ownerAddress),
+      getUserById(body.hirerId as string),
+    ]);
+
+    // Various validations
     if (existingSpace && existingSpace.status === EntityStatus.VALIDATED) {
-      return res.status(401).json({ error: 'You already own a domain' });
+      throw new Error('You already own a domain');
     }
-
-    const builderSpace = await getBuilderPlaceById(body.builderPlaceId as string);
     if (!builderSpace) {
-      return res.status(400).json({ error: "Domain doesn't exist" });
+      throw new Error("Domain doesn't exist");
     }
-
     if (builderSpace.status === EntityStatus.VALIDATED) {
-      return res.status(401).json({ error: 'BuilderPlace already has an owner' });
+      throw new Error('BuilderPlace already has an owner');
     }
-
-    // **** Checks on the Hirer ****
-
-    /**
-     * Check whether the provided address owns a TalentLayer Id
-     */
-    const response = await getTalentLayerUserByAddress(
-      Number(process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID),
-      body.ownerAddress,
-    );
-
-    const talentLayerUser = response?.data?.data?.users[0];
-
     if (!talentLayerUser) {
-      return res.status(401).json({ error: 'Your address does not own a TalentLayer Id' });
+      throw new Error('Your address does not own a TalentLayer Id');
     }
-
-    /**
-     * @notice: Check whether the user already owns a BuilderPlace
-     * @dev: If PENDING profiles with the same address (and / or TalentLayerId), remove address & ID
-     * from pending profile to avoid conflicts on "unique" constraints
-     */
-    const existingProfileWithSameAddress = await getUserByAddress(body.ownerAddress);
+    if (!userProfile) {
+      throw new Error("Profile doesn't exist");
+    }
     if (
       existingProfileWithSameAddress &&
       existingProfileWithSameAddress.status === EntityStatus.VALIDATED &&
@@ -76,62 +79,49 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     }
 
     /**
-     * @dev: Checks whether the user exists in the database
+     * @dev: If profile Validated and owner already set, skip the owner setting step
      */
-    const userProfile = await getUserById(body.hirerId as string);
-    if (!userProfile) {
-      return res.status(400).json({ error: "Profile doesn't exist" });
-    }
-
-    try {
+    if (
+      userProfile.status === EntityStatus.VALIDATED &&
+      userProfile.talentLayerId === body.ownerTalentLayerId &&
+      userProfile.address?.toLocaleLowerCase() === body.ownerAddress.toLocaleLowerCase()
+    ) {
       /**
-       * @dev: If profile Validated and owner already set, skip the owner setting step
+       * @dev: Remove owner from pending domain to avoid conflicts on field "unique" constraint
        */
-      if (
-        userProfile.status === EntityStatus.VALIDATED &&
-        userProfile.talentLayerId === body.ownerTalentLayerId &&
-        userProfile.address?.toLocaleLowerCase() === body.ownerAddress.toLocaleLowerCase()
-      ) {
-        /**
-         * @dev: Remove owner from pending domain to avoid conflicts on field "unique" constraint
-         */
-        if (
-          existingSpace &&
-          existingSpace.ownerId &&
-          existingSpace.status === EntityStatus.PENDING
-        ) {
-          await removeBuilderPlaceOwner({
-            id: existingSpace.id,
-            ownerId: existingSpace.ownerId,
-          });
-        }
-
-        await setBuilderPlaceOwner({
-          id: body.builderPlaceId,
-          ownerId: body.hirerId,
-        });
-      } else {
-        /**
-         * @dev: If existing pending profile with same address, remove address
-         * from pending profile to avoid conflicts on field "unique" constraint
-         */
-        if (
-          existingProfileWithSameAddress &&
-          existingProfileWithSameAddress.status === EntityStatus.PENDING
-        ) {
-          //TODO: Prisma carrément suppr le user ?
-          await removeOwnerFromUser(existingProfileWithSameAddress.id.toString());
-        }
-
-        /**
-         * @dev: Set Hirer profile owner
-         */
-        await setUserOwner({
-          id: body.hirerId,
-          userAddress: body.ownerAddress.toLocaleLowerCase(),
-          talentLayerId: talentLayerUser.id,
+      if (existingSpace && existingSpace.ownerId && existingSpace.status === EntityStatus.PENDING) {
+        //TODO: Prisma carrément suppr la BP ?
+        await removeBuilderPlaceOwner({
+          id: existingSpace.id,
+          ownerId: existingSpace.ownerId,
         });
       }
+
+      await setBuilderPlaceOwner({
+        id: body.builderPlaceId,
+        ownerId: body.hirerId,
+      });
+    } else {
+      /**
+       * @dev: If existing pending profile with same address, remove address
+       * from pending profile to avoid conflicts on field "unique" constraint
+       */
+      if (
+        existingProfileWithSameAddress &&
+        existingProfileWithSameAddress.status === EntityStatus.PENDING
+      ) {
+        //TODO: Prisma carrément suppr le user ?
+        await removeOwnerFromUser(existingProfileWithSameAddress.id.toString());
+      }
+
+      /**
+       * @dev: Set Hirer profile owner
+       */
+      await setUserOwner({
+        id: body.hirerId,
+        userAddress: body.ownerAddress.toLocaleLowerCase(),
+        talentLayerId: talentLayerUser.id,
+      });
 
       /**
        * @dev: Remove owner from pending domain to avoid conflicts on field "unique" constraint
@@ -147,16 +137,14 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         id: body.builderPlaceId,
         ownerId: body.hirerId,
       });
-
-      res.status(200).json({
-        message: 'BuilderPlace domain & Hirer profile updated successfully',
-        builderPlaceId: body.builderPlaceId,
-        hirerId: body.hirerId,
-      });
-    } catch (error: any) {
-      res.status(400).json({ error: error.message });
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
+
+    res.status(200).json({
+      message: 'BuilderPlace domain & Hirer profile updated successfully',
+      builderPlaceId: body.builderPlaceId,
+      hirerId: body.hirerId,
+    });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
   }
 }
