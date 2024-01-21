c3abf8c85dd07a96d1daa1f3fc2b0b57a19d51c1
Quentin D.C <quent043@hotmail.com>
2024-01-21 13:18:46 +0000

    API optim set-builder-place-and-hirer-owner.ts

--- src/pages/api/domain/set-builder-place-and-hirer-owner.ts
+++ src/pages/api/domain/set-builder-place-and-hirer-owner.ts
@@ -14,11 +14,11 @@ import {
   removeOwnerFromUser,
   setUserOwner,
 } from '../../../modules/BuilderPlace/actions/user';
-import { MISSING_DATA } from '../../../modules/BuilderPlace/apiResponses';
+import { METHOD_NOT_ALLOWED, MISSING_DATA } from '../../../modules/BuilderPlace/apiResponses';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'PUT') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: METHOD_NOT_ALLOWED });
   }
 
   const body: SetBuilderPlaceAndHirerOwner = req.body;
