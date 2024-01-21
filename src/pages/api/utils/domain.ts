a436bc11b7b2b13e088c667efe30c4c482bf8121
Quentin D.C <quent043@hotmail.com>
2024-01-21 13:22:07 +0000

    Added try catch in signature checks

--- src/pages/api/utils/domain.ts
+++ src/pages/api/utils/domain.ts
@@ -18,21 +18,27 @@ export const checkSignature = async (
   signature: `0x${string}` | Uint8Array,
   res: NextApiResponse,
 ) => {
-  const address = await recoverMessageAddress({
-    message: id,
-    signature: signature,
-  });
+  try {
+    const address = await recoverMessageAddress({
+      message: id,
+      signature: signature,
+    });
 
-  const builderPlace = await getBuilderPlaceByCollaboratorAddressAndId(
-    address.toLocaleLowerCase(),
-    id,
-  );
+    const builderPlace = await getBuilderPlaceByCollaboratorAddressAndId(
+      address.toLocaleLowerCase(),
+      id,
+    );
 
-  if (!builderPlace) {
-    return res.status(400).json({ error: 'No BuilderPlace found.' });
+    if (!builderPlace) {
+      return res.status(400).json({ error: 'No BuilderPlace found.' });
+    }
+
+    return { builderPlace, address };
+  } catch (error) {
+    console.error('Error in checkSignature:', error);
+
+    return res.status(500).json({ error: 'An error occurred while verifying the signature.' });
   }
-
-  return { builderPlace, address };
 };
 
 /**
@@ -50,21 +56,27 @@ export const checkOwnerSignature = async (
   signature: `0x${string}` | Uint8Array,
   res: NextApiResponse,
 ) => {
-  const address = await recoverMessageAddress({
-    message: ownerId,
-    signature: signature,
-  });
+  try {
+    const address = await recoverMessageAddress({
+      message: ownerId,
+      signature: signature,
+    });
 
-  const builderPlace = await getBuilderPlaceByOwnerTlIdAndId(ownerId, builderPlaceId);
+    const builderPlace = await getBuilderPlaceByOwnerTlIdAndId(ownerId, builderPlaceId);
 
-  if (
-    builderPlace &&
-    address.toLocaleLowerCase() !== builderPlace?.owner?.address?.toLocaleLowerCase()
-  ) {
-    return res.status(401).json({ error: 'Not BuilderPlace owner' });
+    if (
+      builderPlace &&
+      address.toLocaleLowerCase() !== builderPlace?.owner?.address?.toLocaleLowerCase()
+    ) {
+      return res.status(401).json({ error: 'Not BuilderPlace owner' });
+    }
+
+    return { builderPlace, address };
+  } catch (error) {
+    console.error('Error in checkSignature:', error);
+
+    return res.status(500).json({ error: 'An error occurred while verifying the signature.' });
   }
-
-  return { builderPlace, address };
 };
 
 export const isCollaboratorExists = (
