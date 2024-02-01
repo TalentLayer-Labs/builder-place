4bac68000ae6afadcc7583daf2e8e15526c51b04
Romain <romain@talentlayer.org>
2024-02-01 11:46:42 +0100

    improve UI

--- src/components/onboarding/Layout.tsx
+++ src/components/onboarding/Layout.tsx
@@ -15,7 +15,7 @@ function Layout({ children, className, step }: ContainerProps) {
     <div className=''>
       <header className='navbar w-full border-stroke bg-white duration-300'>
         <div className='container relative lg:max-w-[1305px] lg:px-10'>
-          <div className='flex items-center justify-between h-[80px]'>
+          <div className='flex items-center justify-between h-[80px] px-2'>
             <div className='block py-4 lg:py-0'>
               <Link href='/' className='block max-w-[145px] sm:max-w-[200px]'>
                 <Image src='/logo-text-dark.png' alt='logo' width={256} height={41} />
--- src/components/onboarding/Steps.tsx
+++ src/components/onboarding/Steps.tsx
@@ -8,31 +8,31 @@ function Steps({ currentStep }: { currentStep: number }) {
   };
 
   return (
-    <div className='mt-6 hidden sm:flex justify-center items-center '>
-      <div className='w-full md:w-3/4 border-2 bg-base-100 border-solid border-opacity-10 border-info rounded-2xl p-4 mx-6'>
-        <div className='flex flex-col sm:flex-row'>
-          <div className='sm:flex-1 border-r border-dark bg-base-100 flex sm:mb-0 mb-2 items-center justify-center'>
+    <div className='mt-6 flex justify-center items-center '>
+      <div className='flex-1 md:flex-initial md:w-3/4 border-2 bg-base-100 border-solid border-opacity-10 border-info rounded-2xl p-3 sm:p-4 mx-4 sm:mx-6'>
+        <div className='flex flex-row'>
+          <div className='flex-1 border-r border-dark bg-base-100 flex items-center justify-center'>
             <div
-              className={`w-12 h-12 p-2 rounded-full flex items-center justify-center ${getCircleColor(
+              className={`w-10 h-10 sm:w-12 sm:h-12 p-2 rounded-full flex items-center justify-center ${getCircleColor(
                 1,
               )}`}>
-              <div className='text-white text-xl font-semibold'>1</div>
+              <div className='text-white text-md sm:text-xl font-semibold'>1</div>
             </div>
             <div className='ml-3 flex flex-col'>
-              <div className='text-light text-xl font-semibold leading-6'>create</div>
-              <div className='text-light text-sm'>your profile</div>
+              <div className='text-light text-md sm:text-xl font-semibold leading-6'>create</div>
+              <div className='text-light text-xs sm:text-sm'>your profile</div>
             </div>
           </div>
-          <div className='sm:flex-1 bg-base-100 flex sm:mb-0 mb-2 items-center justify-center'>
+          <div className='flex-1 bg-base-100 flex items-center justify-center'>
             <div
-              className={`w-12 h-12 p-2 rounded-full flex items-center justify-center ${getCircleColor(
+              className={`w-10 h-10 sm:w-12 sm:h-12 p-2 rounded-full flex items-center justify-center ${getCircleColor(
                 2,
               )}`}>
-              <div className='text-white text-xl font-semibold'>2</div>
+              <div className='text-white text-md sm:text-xl font-semibold'>2</div>
             </div>
             <div className='ml-3 flex flex-col'>
-              <div className='text-light text-xl font-semibold leading-6'>create</div>
-              <div className='text-light text-sm'>your platform</div>
+              <div className='text-light text-md sm:text-xl font-semibold leading-6'>create</div>
+              <div className='text-light text-xs sm:text-sm'>your platform</div>
             </div>
           </div>
         </div>
