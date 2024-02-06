e99130844e0e90ff14d0a410f3649a6b390ec236
Romain <romain@talentlayer.org>
2024-02-06 16:13:39 +0100

    init function

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,22 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const {
-    query: { id },
-    method,
-  } = req;
-
-  switch (method) {
-    case 'PUT':
-      // TODO: Update the user with the given id
-      res.status(200).json({ message: `User ${id} updated` });
-      break;
-    case 'DELETE':
-      // TODO: Delete the user with the given id
-      res.status(200).json({ message: `User ${id} deleted` });
-      break;
-    default:
-      res.setHeader('Allow', ['PUT', 'DELETE']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
+export async function PUT(req: Request) {
+  // TODO: implement PUT
 }
