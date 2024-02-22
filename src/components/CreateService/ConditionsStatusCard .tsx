50985f4ca28565afa81eb890fbafb10117c75078
Quentin D.C <quent043@hotmail.com>
2024-02-22 17:16:58 +0400

    Updated ConditionsStatusCard .tsx

--- src/components/CreateService/ConditionsStatusCard .tsx
+++ src/components/CreateService/ConditionsStatusCard .tsx
@@ -1,34 +1,55 @@
 import { IReturnPostingCondition } from '../../hooks/useCheckPostConditions';
-import { PostingCondition } from '../../modules/BuilderPlace/types';
+import { ChainIdEnum, PostingCondition } from '../../modules/BuilderPlace/types';
+
+//TODO write the good links
+const blockScanList = {
+  1: 'https://etherscan.io/',
+  42161: 'https://ropsten.etherscan.io/',
+  137: 'https://rinkeby.etherscan.io/',
+  134: 'https://goerli.etherscan.io/',
+  56: 'https://kovan.etherscan.io/',
+};
 
 const ConditionsStatusCard = ({ condition, validated }: IReturnPostingCondition) => {
+  const getBlockExplorerLink = (chainId: ChainIdEnum, address: string) => {
+    const baseLink = blockScanList[chainId];
+    return baseLink ? `${baseLink}address/${address}` : '';
+  };
+
   const renderConditionText = (condition: PostingCondition) => {
     switch (condition.type) {
       case 'NFT':
-        return (
-          <span>
-            You need to own an NFT from the <strong>{condition.contractName}</strong> contract
-          </span>
-        );
+        return `You need to own an NFT from the ${condition.contractName} contract`;
       case 'Token':
-        return (
-          <span>
-            You need a minimum of{' '}
-            <strong>
-              {condition.minimumAmount} {condition.tokenSign}
-            </strong>{' '}
-            from the <strong>{condition.contractName}</strong> contract
-          </span>
-        );
+        return `You need a minimum of ${condition.minimumAmount} ${condition.tokenSign} from the ${condition.contractName} contract`;
       default:
         return 'Unknown condition';
     }
   };
 
+  const renderButton = () => {
+    if (!validated) {
+      const buttonText = condition.type === 'Token' ? `Buy ${condition.tokenSign}` : 'Mint NFT';
+      const blockExplorerLink = getBlockExplorerLink(condition.chainId, condition.address);
+
+      return (
+        blockExplorerLink && (
+          <a href={blockExplorerLink} target='_blank' rel='noopener noreferrer' className='ml-2'>
+            <button className='bg-primary text-primary py-1 px-2 rounded'>{buttonText}</button>
+          </a>
+        )
+      );
+    }
+    return null;
+  };
+
   return (
     <li className='flex items-center mb-2'>
       <span className={validated ? 'text-green-500' : 'text-red-500'}>{validated ? '✓' : '✕'}</span>
-      <p className='ml-2'>{renderConditionText(condition)}</p>
+      <div className='flex flex-grow items-center justify-between ml-2'>
+        <p>{renderConditionText(condition)}</p>
+        {renderButton()}
+      </div>
     </li>
   );
 };
