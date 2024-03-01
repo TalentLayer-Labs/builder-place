2b9b1bc4d17103dc98cba982cbf25ed23395da98
Quentin D.C <quent043@hotmail.com>
2024-03-01 13:03:27 +0400

    Added hook

---
+++ src/hooks/usePlatformByOwnerAddress.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { IPlatform } from '../types';
+import useTalentLayerClient from './useTalentLayerClient';
+
+const usePlatformByOwner = (address?: `0x${string}`): IPlatform | null => {
+  const [platforms, setAddresses] = useState<IPlatform | null>(null);
+  const talentLayerClient = useTalentLayerClient();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (address) {
+        try {
+          const response = await talentLayerClient?.platform.getByOwner(address);
+          //TODO .length check ?
+          setAddresses(response[0]);
+        } catch (error: any) {
+          // eslint-disable-next-line no-console
+          console.error(error);
+        }
+      }
+    };
+    fetchData();
+  }, [address]);
+
+  return platforms;
+};
+
+export default usePlatformByOwner;
