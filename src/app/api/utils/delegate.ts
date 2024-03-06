0d583e085faf85f7b4d3ca14aad1a1e4813d77b6
Quentin D.C <quent043@hotmail.com>
2024-03-06 17:17:49 +0400

    Added utils functions

---
+++ src/app/api/utils/delegate.ts
@@ -0,0 +1,47 @@
+import { getUserByAddress } from '../../../queries/users';
+import { mnemonicToAccount } from 'viem/accounts';
+import { createPublicClient, createWalletClient, http, PublicClient } from 'viem';
+import { getViemFormattedChain } from '../../../chains';
+import { WalletClient } from 'wagmi';
+import { NetworkEnum } from '../../../types';
+
+export async function isPlatformAllowedToDelegate(
+  chainId: number,
+  userAddress: string,
+): Promise<boolean> {
+  const getUser = await getUserByAddress(chainId, userAddress);
+  const delegateAddresses: string[] = getUser.data?.data?.users[0]?.delegates.map((delegate: any) =>
+    delegate.address.toLowerCase(),
+  );
+
+  return !(
+    process.env.NEXT_PUBLIC_DELEGATE_ADDRESS &&
+    delegateAddresses.indexOf(process.env.NEXT_PUBLIC_DELEGATE_ADDRESS.toLowerCase()) === -1
+  );
+}
+
+export async function getDelegationSigner(): Promise<WalletClient | null> {
+  const delegateSeedPhrase = process.env.NEXT_PRIVATE_DELEGATE_SEED_PHRASE;
+
+  if (delegateSeedPhrase) {
+    const account = mnemonicToAccount(delegateSeedPhrase);
+    return createWalletClient({
+      account,
+      chain: getViemFormattedChain(
+        process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID as unknown as NetworkEnum,
+      ),
+      transport: http(),
+    });
+  } else {
+    return null;
+  }
+}
+
+export function getPublicClient(): PublicClient {
+  return createPublicClient({
+    chain: getViemFormattedChain(
+      process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID as unknown as NetworkEnum,
+    ),
+    transport: http(),
+  });
+}
