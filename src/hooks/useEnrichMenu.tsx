96d07056d2a5ab031283bf3a62992f4ff5cae8f9
Quentin D.C <quent043@hotmail.com>
2024-03-08 11:12:21 +0400

    Added async in hook

--- src/hooks/useEnrichMenu.tsx
+++ src/hooks/useEnrichMenu.tsx
@@ -5,28 +5,32 @@ import { PlusCircleIcon } from '@heroicons/react/24/outline';
 const useEnrichMenu = (isPostingAllowed: boolean = false, isWorker: boolean) => {
   const [enrichedWorkerNavigation, setEnrichedWorkerNavigation] =
     useState<MenuItem[]>(workerNavigation);
-  const [menuLoading, setMenuLoading] = useState(false);
+  const [menuLoading, setMenuLoading] = useState(isWorker);
 
   useEffect(() => {
-    if (isWorker) {
-      setMenuLoading(true);
-      if (isPostingAllowed) {
-        const newMissionItem = {
-          name: 'new mission',
-          href: '/work/create',
-          icon: PlusCircleIcon,
-          current: false,
-        };
+    const enrichMenu = async () => {
+      if (isWorker) {
+        setMenuLoading(true);
+        if (isPostingAllowed) {
+          const newMissionItem = {
+            name: 'new mission',
+            href: '/work/create',
+            icon: PlusCircleIcon,
+            current: false,
+          };
 
-        // Check if 'new mission' item already exists to prevent duplicates
-        if (!enrichedWorkerNavigation.some(item => item.name === newMissionItem.name)) {
-          setEnrichedWorkerNavigation([...workerNavigation, newMissionItem]);
+          // Check if 'new mission' item already exists to prevent duplicates
+          if (!enrichedWorkerNavigation.some(item => item.name === newMissionItem.name)) {
+            setEnrichedWorkerNavigation([...workerNavigation, newMissionItem]);
+          }
+        } else {
+          setEnrichedWorkerNavigation(workerNavigation);
         }
-      } else {
-        setEnrichedWorkerNavigation(workerNavigation);
+        setMenuLoading(false);
       }
-      setMenuLoading(false);
-    }
+    };
+
+    enrichMenu();
   }, [isPostingAllowed, menuLoading]);
 
   return {
