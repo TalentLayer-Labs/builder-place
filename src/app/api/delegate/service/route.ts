4685186ec7b93898506a61192681ad5c16a51c8e
Quentin D.C <quent043@hotmail.com>
2024-03-20 10:50:04 +0000

    Create service route

--- src/app/api/delegate/service/route.ts
+++ src/app/api/delegate/service/route.ts
@@ -7,13 +7,9 @@ import {
 } from '../../../utils/delegate';
 import TalentLayerService from '../../../../contracts/ABI/TalentLayerService.json';
 import { getServiceSignature } from '../../../../utils/signature';
-import { getUserById } from '../../../../queries/users';
-import {
-  checkOrResetTransactionCounter,
-  checkUserEmailVerificationStatus,
-  incrementWeeklyTransactionCounter,
-} from '../../../utils/email';
+import { checkUserEmailVerificationStatus } from '../../../utils/email';
 import { getPlatformServicePostingFee } from '../../../../queries/platform';
+import { getUserByTalentLayerId } from '../../../../modules/BuilderPlace/actions/user';
 
 export interface ICreateService {
   chainId: number;
@@ -31,64 +27,81 @@ export async function POST(req: Request) {
   console.log('POST');
   const body: ICreateService = await req.json();
   console.log('json', body);
-  const config = getConfig(body.chainId);
+  const { chainId, userId, userAddress, cid, platformId, signature } = body;
 
-  if (process.env.NEXT_PUBLIC_ACTIVE_DELEGATE !== 'true') {
+  const config = getConfig(chainId);
+
+  if (process.env.NEXT_PUBLIC_ACTIVATE_DELEGATE === 'true') {
     return Response.json({ message: 'Delegation is not activated' }, { status: 500 });
   }
 
   // @TODO: move it to a middleware and apply to all POST and PUT ?
   const signatureAddress = await recoverMessageAddress({
-    message: `connect with ${body.userAddress}`,
-    signature: body.signature,
+    message: `connect with ${userAddress}`,
+    signature: signature,
   });
 
-  if (signatureAddress !== body.userAddress) {
+  if (signatureAddress !== userAddress) {
     return Response.json({ error: 'Invalid signature' }, { status: 401 });
   }
 
   try {
-    const user = await getUserById(body.chainId, body.userId);
+    const user = await getUserByTalentLayerId(userId);
 
     if (user) {
-      checkUserEmailVerificationStatus(user);
+      // return checkUserEmailVerificationStatus(user);
+      const emailResponse = checkUserEmailVerificationStatus(user);
 
-      await Promise.all([
-        checkOrResetTransactionCounter(user),
-        isPlatformAllowedToDelegate(body.chainId, body.userAddress),
+      if (emailResponse) {
+        return emailResponse;
+      }
+
+      const responses = await Promise.all([
+        //TODO problem with database value: need to switch to date => Bigint Not serializable
+        // checkOrResetTransactionCounter(user),
+        isPlatformAllowedToDelegate(chainId, userAddress),
       ]);
+
+      if (responses[0]) {
+        return responses[0];
+      }
+
+      const walletClient = await getDelegationSigner();
+      if (!walletClient) {
+        return;
+      }
+
+      const publicClient = getPublicClient();
+      if (!publicClient) {
+        return;
+      }
+
+      let transaction;
+
+      //TODO: V2 - RPC call instead ?
+      const platformFeesResponse = await getPlatformServicePostingFee(chainId, platformId);
+      let servicePostingFee = platformFeesResponse?.data?.data?.platform.servicePostingFee;
+      servicePostingFee = BigInt(Number(servicePostingFee) || '0');
+
+      const signature = await getServiceSignature({
+        profileId: Number(userId),
+        cid: cid,
+      });
+
+      console.log('Creating service with args', userId, platformId, cid, signature);
+
+      transaction = await walletClient.writeContract({
+        address: config.contracts.serviceRegistry,
+        abi: TalentLayerService.abi,
+        functionName: 'createService',
+        args: [userId, platformId, cid, signature],
+        value: servicePostingFee,
+      });
+
+      // await incrementWeeklyTransactionCounter(user);
+
+      return Response.json({ transaction: transaction }, { status: 201 });
     }
-
-    const walletClient = await getDelegationSigner();
-    if (!walletClient) {
-      return;
-    }
-
-    const publicClient = getPublicClient();
-    if (!publicClient) {
-      return;
-    }
-
-    let transaction;
-
-    //TODO: RPC call instead ?
-    const platformFeesResponse = await getPlatformServicePostingFee(body.chainId, body.platformId);
-    let servicePostingFee = platformFeesResponse?.data?.platform.servicePostingFee;
-    servicePostingFee = BigInt(servicePostingFee || '0');
-
-    const signature = await getServiceSignature({ profileId: Number(body.userId), cid: body.cid });
-
-    transaction = await walletClient.writeContract({
-      address: config.contracts.serviceRegistry,
-      abi: TalentLayerService.abi,
-      functionName: 'createService',
-      args: [body.userId, body.platformId, body.cid, signature],
-      value: servicePostingFee,
-    });
-
-    await incrementWeeklyTransactionCounter(user);
-
-    return Response.json({ transaction: transaction }, { status: 201 });
   } catch (error: any) {
     let message = 'Failed to create service';
     return Response.json({ message, error }, { status: 500 });
