cfc6520013c7d565a1ccc73245c31fef2204f491
Quentin D.C <quent043@hotmail.com>
2024-03-21 17:20:43 +0000

    Replaces BP platform id in context

--- src/context/talentLayer.tsx
+++ src/context/talentLayer.tsx
@@ -69,7 +69,7 @@ const TalentLayerProvider = ({ children }: { children: ReactNode }) => {
         clientSecret: process.env.NEXT_PUBLIC_IPFS_SECRET as string,
         baseUrl: process.env.NEXT_PUBLIC_IPFS_WRITE_URL as string,
       },
-      platformId: parseInt(process.env.NEXT_PUBLIC_PLATFORM_ID as string),
+      platformId: parseInt(builderPlace?.talentLayerPlatformId as string),
       signatureApiUrl: process.env.NEXT_PUBLIC_SIGNATURE_API_URL as string,
       walletConfig: walletClient
         ? {
