4d4cef13ab49f609d0a7e07169e962acc5776ee9
Quentin D.C <quent043@hotmail.com>
2024-03-29 18:49:28 +0000

    Added delegate user update route

---
+++ src/app/api/delegate/user/[id]/route.ts
@@ -0,0 +1,98 @@
+import { recoverMessageAddress } from 'viem';
+import { getConfig } from '../../../../../config';
+import {
+  getDelegationSigner,
+  getPublicClient,
+  isPlatformAllowedToDelegate,
+} from '../../../../utils/delegate';
+import TalentLayerId from '../../../../../contracts/ABI/TalentLayerId.json';
+import {
+  getUserByAddress,
+  getUserByTalentLayerId,
+} from '../../../../../modules/BuilderPlace/actions/user';
+import { ERROR_EMAIL_NOT_VERIFIED } from '../../../../../modules/BuilderPlace/apiResponses';
+import {
+  checkOrResetTransactionCounter,
+  incrementWeeklyTransactionCounter,
+} from '../../../../utils/email';
+
+export interface IUpdateProfile {
+  chainId: number;
+  userAddress: string;
+  cid: string;
+  signature: `0x${string}` | Uint8Array;
+}
+
+/**
+ * PUT /api/delegate/user
+ */
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  console.log('PUT');
+  const body: IUpdateProfile = await req.json();
+  console.log('json', body);
+  const { chainId, userAddress, cid, signature } = body;
+
+  const config = getConfig(chainId);
+
+  if (process.env.NEXT_PUBLIC_ACTIVATE_DELEGATE !== 'true') {
+    return Response.json({ message: 'Delegation is not activated' }, { status: 500 });
+  }
+
+  const signatureAddress = await recoverMessageAddress({
+    message: `connect with ${userAddress}`,
+    signature: signature,
+  });
+
+  const user = await getUserByAddress(signatureAddress);
+
+  if (user?.talentLayerId !== params.id) {
+    return Response.json({ error: 'Invalid signature' }, { status: 401 });
+  }
+
+  try {
+    const user = await getUserByTalentLayerId(params.id);
+
+    if (user) {
+      if (!user.isEmailVerified) {
+        console.log('Email not verified');
+        return Response.json({ error: ERROR_EMAIL_NOT_VERIFIED }, { status: 401 });
+      }
+
+      await checkOrResetTransactionCounter(user);
+      const canDelegate = await isPlatformAllowedToDelegate(chainId, userAddress);
+
+      if (!canDelegate) {
+        Response.json({ error: 'Delegation is Not activated for this address' }, { status: 401 });
+      }
+
+      const walletClient = await getDelegationSigner();
+      if (!walletClient) {
+        console.log('Wallet client not found');
+        return Response.json({ error: 'Server Error' }, { status: 500 });
+      }
+
+      const publicClient = getPublicClient();
+      if (!publicClient) {
+        console.log('Public client not found');
+        return Response.json({ error: 'Server Error' }, { status: 500 });
+      }
+
+      let transaction;
+
+      console.log('Updating profile with args', params.id, cid, signature);
+      transaction = await walletClient.writeContract({
+        address: config.contracts.talentLayerId,
+        abi: TalentLayerId.abi,
+        functionName: 'updateProfileData',
+        args: [params.id, cid],
+      });
+
+      await incrementWeeklyTransactionCounter(user);
+
+      return Response.json({ transaction: transaction }, { status: 201 });
+    }
+  } catch (error: any) {
+    let message = 'Failed to update proposal';
+    return Response.json({ message, error }, { status: 500 });
+  }
+}
