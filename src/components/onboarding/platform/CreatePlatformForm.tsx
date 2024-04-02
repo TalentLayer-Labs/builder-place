6aac5b862d58e25e062176ef62cc3f7d75536f9c
Quentin D.C <quent043@hotmail.com>
2024-04-02 15:21:16 +0100

    Extracted component

--- src/components/onboarding/platform/CreatePlatformForm.tsx
+++ src/components/onboarding/platform/CreatePlatformForm.tsx
@@ -1,6 +1,6 @@
 import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import * as Yup from 'yup';
 import usePlatformByOwner from '../../../hooks/usePlatformByOwnerAddress';
 import UserContext from '../../../modules/BuilderPlace/context/UserContext';
@@ -14,8 +14,7 @@ import UploadImage from '../../UploadImage';
 import AccessDenied from './AccessDenied';
 import { PlatformNameInput } from './PlatformNameInput';
 import useGetPlatformByOwnerId from '../../../modules/BuilderPlace/hooks/platform/useGetPlatform';
-import { useRouter } from 'next/router';
-import AsyncButton from '../../AsyncButton';
+import AlreadyOwnsPlatform from './AlreadyOwnsPlatform';
 
 export interface ICreatePlatformFormValues {
   name: string;
@@ -51,11 +50,10 @@ function CreatePlatformForm({ onSuccess }: { onSuccess: (subdomain: string) => v
   const { open: openConnectModal } = useWeb3Modal();
   const existingTalentLayerPlatform = usePlatformByOwner(address);
   const { createNewPlatform } = useCreatePlatform();
-  const router = useRouter();
-  const [redirecting, setRedirecting] = useState(false);
   const { platform: existingDatabasePlatform, loading: platformDataLoading } =
     useGetPlatformByOwnerId(user?.id);
   const alreadyOwnsAPlatform = existingDatabasePlatform?.ownerId === user?.id;
+  const domain = existingDatabasePlatform?.customDomain || existingDatabasePlatform?.subdomain;
 
   const initialValues: ICreatePlatformFormValues = {
     name: '',
@@ -115,50 +113,7 @@ function CreatePlatformForm({ onSuccess }: { onSuccess: (subdomain: string) => v
   }
 
   if (alreadyOwnsAPlatform) {
-    const domain = existingDatabasePlatform?.customDomain || existingDatabasePlatform?.subdomain;
-
-    const onRedirect = async () => {
-      setRedirecting(true);
-      try {
-        await router.push(`${window.location.protocol}//${domain}`);
-      } catch (error: any) {
-        console.error('Error redirecting', error);
-        setRedirecting(false);
-      }
-    };
-
-    return (
-      <div className='flex flex-col items-center justify-center text-base-content text-primary'>
-        <div className='rounded-lg pb-5 max-w-md text-center'>
-          <p className='text-lg mb-4'>
-            You already own <strong>{domain}</strong>
-          </p>
-          {existingDatabasePlatform?.logo && (
-            <div className='mb-4'>
-              <img
-                src={existingDatabasePlatform?.logo}
-                alt='Platform Logo'
-                className='mx-auto h-20 w-20 object-cover rounded-full border-2'
-              />
-            </div>
-          )}
-          <div className='w-full flex justify-center'>
-            <AsyncButton
-              isSubmitting={redirecting}
-              disabled={redirecting}
-              validateButtonCss={
-                'rounded-md bg-info px-3.5 py-2.5 text-sm font-semibold text-base-content shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-              }
-              loadingButtonCss={
-                'opacity-50 rounded-md bg-info px-3.5 py-2.5 text-sm font-semibold text-base-content shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-              }
-              label={'Go to My Domain'}
-              onClick={onRedirect}
-            />
-          </div>
-        </div>
-      </div>
-    );
+    return <AlreadyOwnsPlatform domain={domain} logo={existingDatabasePlatform?.logo} />;
   }
 
   return (
