1b6873f8e8ff638ec3ff05f0e84fda39ef76b045
Romain <romain@talentlayer.org>
2024-04-04 11:46:37 +0200

    update default seo + share

--- src/pages/[domain]/work/[id]/index.tsx
+++ src/pages/[domain]/work/[id]/index.tsx
@@ -7,6 +7,8 @@ import ServiceDetail from '../../../../components/ServiceDetail';
 import useServiceById from '../../../../hooks/useServiceById';
 import BuilderPlaceContext from '../../../../modules/BuilderPlace/context/BuilderPlaceContext';
 import { sharedGetServerSideProps } from '../../../../utils/sharedGetServerSideProps';
+import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return sharedGetServerSideProps(context);
@@ -27,14 +29,20 @@ function Service() {
   }
 
   return (
-    <div className='max-w-7xl mx-auto text-base-content'>
-      <div className='-mx-6 -mt-6 sm:mx-0 sm:mt-0'>
-        <p className='flex py-2 items-center text-2xl font-bold tracking-wider mb-6 w-full px-6 sm:px-0 mt-6 '>
-          mission
-        </p>
+    <>
+      <NextSeo
+        title={`Mission | ${service?.description?.title}`}
+        description={`check work mission on ${builderPlace?.name} BuilderPlace`}></NextSeo>
+
+      <div className='max-w-7xl mx-auto text-base-content'>
+        <div className='-mx-6 -mt-6 sm:mx-0 sm:mt-0'>
+          <p className='flex py-2 items-center text-2xl font-bold tracking-wider mb-6 w-full px-6 sm:px-0 mt-6 '>
+            mission
+          </p>
+        </div>
+        {service ? <ServiceDetail service={service} /> : <Loading />}
       </div>
-      {service ? <ServiceDetail service={service} /> : <Loading />}
-    </div>
+    </>
   );
 }
 
