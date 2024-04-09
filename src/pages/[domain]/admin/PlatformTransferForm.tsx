cad2bfa31957a1add9bea4fad53de69666635a44
Quentin D.C <quent043@hotmail.com>
2024-04-09 18:47:17 +0100

    Added state style

--- src/pages/[domain]/admin/PlatformTransferForm.tsx
+++ src/pages/[domain]/admin/PlatformTransferForm.tsx
@@ -10,6 +10,7 @@ import { getUserBy } from '../../../modules/BuilderPlace/request';
 import UserProfileDisplay from './UserProfileDisplay';
 import { useState } from 'react';
 import { User } from '@prisma/client';
+import Loading from '../../../components/Loading';
 
 export interface IFormValues {
   toAddress: string;
@@ -29,6 +30,8 @@ function PlatformTransferForm({ callback }: { callback?: () => void }) {
   const { address } = useAccount();
   const platform = usePlatformByOwner(address);
   const [returnedUser, setReturnedUser] = useState<User | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [previousValue, setPreviousValue] = useState<string>('');
 
   const onSubmit = async (
     values: IFormValues,
@@ -62,6 +65,8 @@ function PlatformTransferForm({ callback }: { callback?: () => void }) {
         'platform',
       );
 
+      //TODO: Persist in DB (on fait,quoi en cas de problème avec la 2ème tx ?)
+
       if (callback) {
         callback();
       }
@@ -74,10 +79,16 @@ function PlatformTransferForm({ callback }: { callback?: () => void }) {
     }
   };
 
-  const handleInputChange = async (value: string) => {
+  const validateInput = async (value: string) => {
+    if (value === previousValue) return;
+
+    setPreviousValue(value);
     setReturnedUser(undefined);
+    //TODO use React query
     if (value.match(ethAddressRegex)) {
+      setLoading(true);
       const user = await getUserBy({ address: value });
+      setLoading(false);
       console.log('found user', user);
       setReturnedUser(user);
       if (!user) {
@@ -90,6 +101,7 @@ function PlatformTransferForm({ callback }: { callback?: () => void }) {
     <Formik
       initialValues={{ toAddress: '' }}
       validationSchema={validationSchema}
+      validateOnBlur={false}
       onSubmit={onSubmit}>
       {({ isSubmitting }) => (
         <Form>
@@ -99,13 +111,23 @@ function PlatformTransferForm({ callback }: { callback?: () => void }) {
                 type='text'
                 id='toAddress'
                 name='toAddress'
-                className='mt-1 mb-1 block w-full rounded-xl border-2 border-info bg-base-200 shadow-sm focus:ring-opacity-50 mr-4'
+                className={`mt-1 mb-1 block w-full rounded-xl border-2 border-info bg-base-200 ${
+                  loading && 'opacity-50'
+                } shadow-sm focus:ring-opacity-50 mr-4`}
                 placeholder='Transfer to...'
-                validate={handleInputChange}
+                disabled={loading}
+                validate={validateInput}
+                onKeyDown={(e: any) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault(); // Prevent form submission
+                  }
+                }}
               />
-              {/*<SubmitButton isSubmitting={isSubmitting} label='Transfer' />*/}
+              {loading && <Loading />}
             </div>
-            {returnedUser && <UserProfileDisplay user={returnedUser} isSubmitting={isSubmitting} />}
+            {!!returnedUser && (
+              <UserProfileDisplay user={returnedUser} isSubmitting={isSubmitting} />
+            )}
             <span className='text-alone-error'>
               <ErrorMessage name='toAddress' />
             </span>
